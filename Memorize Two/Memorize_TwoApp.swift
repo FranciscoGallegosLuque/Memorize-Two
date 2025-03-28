1bbd455edd40ce69a0e41882aeb3d67eec37a337
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-03-28 19:23:32 -0300

    Initial Commit

---
+++ Memorize Two/Memorize_TwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_TwoApp.swift
+//  Memorize Two
+//
+//  Created by Francisco Manuel Gallegos Luque on 28/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_TwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memorize TwoTests/Memorize_TwoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_TwoTests.swift
+//  Memorize TwoTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 28/03/2025.
+//
+
+import Testing
+@testable import Memorize_Two
+
+struct Memorize_TwoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
